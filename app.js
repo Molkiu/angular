278abeb9cc3c86bdc4d11afdae55105ba90fa67d
Molkiu <alexmartinez@tuta.io>
2017-02-18 22:46:45 +0100

    function on input, filter

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 (function(){
   'use strict';
 
-  angular.module('myFirstApp',[])
+  angular.module('myApp',[])
 
-  .controller('myFirstController', function($scope){
-    $scope.name = "Alex";
+  .controller('UController', UController);
+
+  function UController($scope,$filter){
+    $scope.name = "";
+    $scope.counter = 0;
+
+    $scope.upper = function(){
+      var upCase = $filter('uppercase');
+      $scope.name = upCase($scope.name);
+    };
+
+    $scope.displayNumeric = function(){
+      var inputCount = calculateString($scope.name) ;
+      $scope.counter = inputCount;
+    };
+
+    function calculateString(string){
+      var stringValue = 0;
+      for(var i = 0; i<string.length;i++){
+        stringValue += string.charCodeAt(i);
+      }
+      return stringValue;
+    };
   };
-})());
+})();
